2ab97e0e8ad6760aa379d1cf8c5fc0526a2bb8db
Agvillam <87862630+Agvillam@users.noreply.github.com>
2025-01-17 19:47:30 +0100

    cmabios

---
+++ FPGA/GLOBAL_V1/ESTRUCTURAL_4/DISPLAY.srcs/sources_1/imports/sources_1/new/caso_turnos.vhd
@@ -0,0 +1,38 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;  -- Para operaciones con "unsigned" y "signed"
+use work.TiposComunes.all;
+
+entity caso_turnos is
+   Port (
+        clk           : in  STD_LOGIC;
+        reset         : in  STD_LOGIC;
+        resultado     : out unsigned(9 downto 0);
+        rst_punt_total: out STD_LOGIC
+   );
+end caso_turnos;
+
+architecture Behavioral of caso_turnos is
+    signal turno: unsigned(9 downto 0) := (others => '0');
+begin
+
+    process(reset)
+        
+    begin
+        if  reset = '1'  then
+            if turno = 13 then
+                turno <= (others => '0');  -- Vuelve a 0 cuando llega a 13
+            else
+                turno <= turno + 2;  -- Incrementa el turno
+            end if;
+            
+        end if;
+    
+    end process;
+    
+    resultado <= turno;
+    rst_punt_total <= '1' when turno = 0 else '0'; 
+
+end Behavioral;
+
+
