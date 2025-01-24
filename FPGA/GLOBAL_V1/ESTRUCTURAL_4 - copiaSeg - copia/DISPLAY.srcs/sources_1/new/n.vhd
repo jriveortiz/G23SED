ba19dae3e4ab83fc2425b0590ddf10f0fde1041d
Agvillam <87862630+Agvillam@users.noreply.github.com>
2025-01-24 20:14:35 +0100

    ULTIMA

--- FPGA/GLOBAL_V1/ESTRUCTURAL_4 - copiaSeg - copia/DISPLAY.srcs/sources_1/new/n.vhd
+++
@@ -1,9 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Declaración del package
-package tipos_personalizados is
-    type matriz_vectores is array (7 downto 0) of STD_LOGIC_VECTOR(6 downto 0);
-    type caracter_vector is array (7 downto 3) of character;
-    type jugadores_2 is array (2 downto 1) of STD_LOGIC_VECTOR(13 downto 1);
-end tipos_personalizados;
